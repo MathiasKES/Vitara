e6e43ce86f50bc1ec507dd8f6f1211eff2e4067b
MathiasKES <warlour@eksworld.dk>
2026-03-21 13:06:11 +0100

    fix(main): resolve NameError by recovering deleted imports

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
+import os
+import secrets
+from flask import Blueprint, render_template, redirect, url_for, request, current_app, flash
 from flask_login import login_required, current_user
+from app import db
 
 main_bp = Blueprint('main', __name__)
 
